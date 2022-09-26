2bd2e579b81ab9b75f54c848591835e8be5de65c
Pddro1 <pedrohcamargon@gmail.com>
2022-09-25 22:47:18 -0300

    primeiro commit

---
+++ src/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Article = (props) => {
+    return(
+        <div>
+            <div class="bg-light mb-4 py-3 py-sm-5">Pedro</div>
+        </div>
+    );
+};
+
+export default Article;
